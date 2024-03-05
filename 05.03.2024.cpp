29c766f16d08612165baed271c5c8ab098bfff14
Upendra kumar <89980759+upendra14@users.noreply.github.com>
2024-03-05 23:21:29 +0530

    Update 05.03.2024.cpp

--- 05.03.2024.cpp
+++ 05.03.2024.cpp
@@ -6,18 +6,20 @@ class Solution{
     // Function to find the maximum index difference.
     int maxIndexDiff(int a[], int n) 
     { 
-      int ans=0;
-      int i=0,j=n-1;
-      while(i<=j){
-          if(a[i]<=a[j]){
-             ans=max(ans,j-i);
-            i++;
-            j=n-1;
-          }
-          else if(a[i]>a[j]){
-              j--;
-          } 
-      }
-      return ans;
+      int maxi=0;
+        vector<int>v={0};
+        
+        for(int i=1; i<n; i++){
+            int back=v.size()-1;
+            while(v.size() && a[i]>=a[v[back]] && back>=0){
+                maxi=max(maxi, i-v[back]);
+                back--;
+            }
+            back=v.size()-1;
+            if(!v.size() || a[i]<a[v[back]]){
+                v.push_back(i);
+            }
+        }
+        return maxi; 
     }
 };
